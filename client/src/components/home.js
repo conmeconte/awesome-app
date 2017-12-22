a07f562dc659fd1d8738d8802d7ad058bb892a63
conmeconte <saja523@hotmail.com>
2017-12-22 12:35:54 -0800

    Finished react setup

---
+++ client/src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1>This is the Home!</h1>
+            <p>ipsum dladfjakdl;f adfja;ldsfj asdfadfas</p>
+        </div>
+    )
+}
+
